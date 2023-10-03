201ecaed31045b8a78ce30707afb292d2da612dd
Mari <72758580+Mariana4Alves@users.noreply.github.com>
2023-10-02 21:27:22 -0300

    Add files via upload

---
+++ Atividade4/Ptriangulo/Form1.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ptriangulo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BtnVerificar_Click(object sender, EventArgs e)
+        {
+            double valorA, valorB, valorC;
+
+            if(!double.TryParse(txtA.Text, out valorA) || !double.TryParse(txtB.Text, out valorB) || !double.TryParse(txtC.Text, out valorC))
+                MessageBox.Show("Valores devem ser numéricos");
+            else
+            {
+                if(valorA < (valorB + valorC) && valorA > 
+                    Math.Abs(valorB - valorC) && valorB < (valorA + valorC) 
+                    && valorB > Math.Abs(valorA - valorC)
+                    && valorC < (valorA + valorB) && 
+                    valorC > Math.Abs(valorA - valorB))
+                {
+                    if(valorA == valorB && valorB == valorC)
+                        MessageBox.Show($"Os valores {valorA}, {valorB} e {valorC} formam um triângulo equilátero.");
+                    else
+                    {
+                        if(valorA == valorB || valorA == valorC || valorB == valorC)
+                        {
+                            MessageBox.Show($"Os valores {valorA}, {valorB} e {valorC} formam um triângulo isósceles.");
+                        }
+                        else
+                        {
+                            MessageBox.Show($"Os valores {valorA}, {valorB} e {valorC} formam um triângulo escaleno.");
+                        }
+                    }
+                }
+                else
+                {
+                    MessageBox.Show($"Os valores {valorA}, {valorB} e {valorC} não formam um triângulo.");
+                }
+            }
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            txtA.Clear();
+            txtB.Clear();
+            txtC.Clear();
+            errorProvider1.SetError(txtA, "");
+            errorProvider2.SetError(txtB, "");
+            errorProvider3.SetError(txtC, "");
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void TxtbA_Validated(object sender, EventArgs e)
+        {
+            errorProvider1.SetError(txtA, "");
+            double valorA;
+
+            if(!double.TryParse(txtA.Text, out valorA))
+            {
+                errorProvider1.SetError(txtA, "Valor de A inválido.");
+            }
+        }
+
+        private void TxtbB_Validated(object sender, EventArgs e)
+        {
+            errorProvider2.SetError(txtB, "");
+            double valorB;
+
+            if (!double.TryParse(txtA.Text, out valorB))
+            {
+                errorProvider2.SetError(txtB, "Valor de B inválido.");
+            }
+
+        }
+
+        private void TxtbC_Validated(object sender, EventArgs e)
+        {
+            errorProvider3.SetError(txtC, "");
+            double valorC;
+
+            if (!double.TryParse(txtA.Text, out valorC))
+            {
+                errorProvider3.SetError(txtC, "Valor de C inválido.");
+            }
+
+        }
+    }
+}
---
+++ Atividade4/Ptriangulo/Ptriangulo/Ptriangulo/Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Ptriangulo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnVerificar = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtA = new System.Windows.Forms.TextBox();
+            this.txtB = new System.Windows.Forms.TextBox();
+            this.txtC = new System.Windows.Forms.TextBox();
+            this.lblA = new System.Windows.Forms.Label();
+            this.lbaB = new System.Windows.Forms.Label();
+            this.lblC = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider2 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProvider3 = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnVerificar
+            // 
+            this.btnVerificar.Location = new System.Drawing.Point(150, 403);
+            this.btnVerificar.Name = "btnVerificar";
+            this.btnVerificar.Size = new System.Drawing.Size(145, 88);
+            this.btnVerificar.TabIndex = 4;
+            this.btnVerificar.Text = "Verificar";
+            this.btnVerificar.UseVisualStyleBackColor = true;
+            this.btnVerificar.Click += new System.EventHandler(this.BtnVerificar_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(481, 403);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(145, 88);
+            this.btnLimpar.TabIndex = 5;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(812, 403);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(145, 88);
+            this.btnSair.TabIndex = 6;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // txtA
+            // 
+            this.txtA.Location = new System.Drawing.Point(150, 187);
+            this.txtA.Name = "txtA";
+            this.txtA.Size = new System.Drawing.Size(145, 35);
+            this.txtA.TabIndex = 1;
+            this.txtA.Validated += new System.EventHandler(this.TxtbA_Validated);
+            // 
+            // txtB
+            // 
+            this.txtB.Location = new System.Drawing.Point(481, 187);
+            this.txtB.Name = "txtB";
+            this.txtB.Size = new System.Drawing.Size(145, 35);
+            this.txtB.TabIndex = 2;
+            this.txtB.Validated += new System.EventHandler(this.TxtbB_Validated);
+            // 
+            // txtC
+            // 
+            this.txtC.Location = new System.Drawing.Point(812, 187);
+            this.txtC.Name = "txtC";
+            this.txtC.Size = new System.Drawing.Size(145, 35);
+            this.txtC.TabIndex = 3;
+            this.txtC.Validated += new System.EventHandler(this.TxtbC_Validated);
+            // 
+            // lblA
+            // 
+            this.lblA.AutoSize = true;
+            this.lblA.Location = new System.Drawing.Point(179, 142);
+            this.lblA.Name = "lblA";
+            this.lblA.Size = new System.Drawing.Size(88, 29);
+            this.lblA.TabIndex = 6;
+            this.lblA.Text = "Lado A";
+            // 
+            // lbaB
+            // 
+            this.lbaB.AutoSize = true;
+            this.lbaB.Location = new System.Drawing.Point(510, 142);
+            this.lbaB.Name = "lbaB";
+            this.lbaB.Size = new System.Drawing.Size(89, 29);
+            this.lbaB.TabIndex = 7;
+            this.lbaB.Text = "Lado B";
+            // 
+            // lblC
+            // 
+            this.lblC.AutoSize = true;
+            this.lblC.Location = new System.Drawing.Point(841, 142);
+            this.lblC.Name = "lblC";
+            this.lblC.Size = new System.Drawing.Size(90, 29);
+            this.lblC.TabIndex = 8;
+            this.lblC.Text = "Lado C";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // errorProvider2
+            // 
+            this.errorProvider2.ContainerControl = this;
+            // 
+            // errorProvider3
+            // 
+            this.errorProvider3.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 567);
+            this.Controls.Add(this.lblC);
+            this.Controls.Add(this.lbaB);
+            this.Controls.Add(this.lblA);
+            this.Controls.Add(this.txtC);
+            this.Controls.Add(this.txtB);
+            this.Controls.Add(this.txtA);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnVerificar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVerificar;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtA;
+        private System.Windows.Forms.TextBox txtB;
+        private System.Windows.Forms.TextBox txtC;
+        private System.Windows.Forms.Label lblA;
+        private System.Windows.Forms.Label lbaB;
+        private System.Windows.Forms.Label lblC;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.ErrorProvider errorProvider2;
+        private System.Windows.Forms.ErrorProvider errorProvider3;
+    }
+}
+
